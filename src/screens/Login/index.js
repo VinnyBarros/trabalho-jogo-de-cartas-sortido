f9ac1f4f704470b5fdb20f9477da7e4d0cc8461b
VinnyBarros <vinnybarros07@gmail.com>
2022-07-01 17:56:03 -0300

    jogo-de-cartas-sortido

---
+++ src/screens/Login/index.js
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+      <Button title='ENTRAR'/>
+    </View>
+  )
+}
+
+export default Login
